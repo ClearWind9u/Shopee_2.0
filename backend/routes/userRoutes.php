e0e98013be234da4d02acd96b525e40b57b73ef7
Phạm Thanh Phong <phamthanhphong9u@gmail.com>
2025-04-06 20:43:10 +0700

    update path

--- backend/routes/userRoutes.php
+++ backend/routes/userRoutes.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ . '/../controllers/UserController.php';
+// require_once __DIR__ . '/../controllers/UserController.php';
+require_once('/var/www/html/controllers/UserController.php');
+
 
 function handleUserRoutes($route, $method)
 {
