3f59324fc0c1923842a3e775a8629220d7260255
LangTuDaTinh <quangviet210804@gmail.com>
2025-04-24 21:56:50 +0700

    add-interaction-in-detail-page

--- frontend/src/components/Detail/Detail.jsx
+++ frontend/src/components/Detail/Detail.jsx
@@ -6,16 +6,19 @@ import { UserContext } from "../../context/UserContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Detail.css';
 const productType = [
-  { id: 0, img: '/image/thoitrangnam.webp', label: 'Thời Trang Nam' },
-  { id: 1, img: '/image/dienthoaivaphukien.webp', label: 'Điện Thoại & Phụ Kiện' },
-  { id: 2, img: '/image/thietbidientu.webp', label: 'Thiết bị điện tử' },
-  { id: 3, img: '/image/maytinhlaptop.webp', label: 'Máy Tính & Laptop' },
-  { id: 4, img: '/image/mayanhvaquayphim.webp', label: 'Máy Ảnh & Máy Quay Phim' },
+  { id: 0, img: '/image/thoitrangnam.webp', label: 'Thời Trang Nam', listFields : [{id:0,label:'Màu',field:['Đen','Trắng','Đỏ Đô','Xanh Than']},{id:0,label:'Size',field:['M','L','XL','XXL']}] },
+  { id: 1, img: '/image/dienthoaivaphukien.webp', label: 'Điện Thoại & Phụ Kiện', listFields : [{id:0,label:'Màu1',field:['Đen','Trắng','Đỏ Đô','Xanh Than']},{id:0,label:'Size1',field:['M','L','XL','XXL']}] },
+  { id: 2, img: '/image/thietbidientu.webp', label: 'Thiết bị điện tử', listFields : [{id:0,label:'Màu2',field:['Đen','Trắng','Đỏ Đô','Xanh Than']},{id:0,label:'Size2',field:['M','L','XL','XXL']}] },
+  { id: 3, img: '/image/maytinhlaptop.webp', label: 'Máy Tính & Laptop', listFields : [{id:0,label:'Màu3',field:['Đen','Trắng','Đỏ Đô','Xanh Than']},{id:0,label:'Size3',field:['M','L','XL','XXL']}] },
+  { id: 4, img: '/image/mayanhvaquayphim.webp', label: 'Máy Ảnh & Máy Quay Phim', listFields : [{id:0,label:'Màu4',field:['Đen','Trắng','Đỏ Đô','Xanh Than']},{id:0,label:'Size4',field:['M','L','XL','XXL']}] },
   
 ];
 const Detail = () => {
    const [listType, setListType] = useState(productType);
    const [currentImg, setCurrentImg] = useState(productType[0])
+   const [selectedField, setSelectedField] = useState([])
+   const [qty,setQty] = useState(1)
+   const [qtyLeft,setQtyLeft] = useState(10)
   return (
     <div>
       <div className="navbar">
@@ -71,29 +74,34 @@ const Detail = () => {
           Nhận từ 23 Th04 - 25 Th04<br />
           Miễn phí vận chuyển
         </div>
-        <div className="section">
-          <strong>Màu:</strong>
-          <div className="colors">
-            {["Đen", "Trắng", "Đỏ Đô", "Xanh Than"].map((color, i) => (
-              <button key={i}>{color}</button>
-            ))}
-          </div>
-        </div>
-        <div className="section">
-          <strong>Size:</strong>
-          <div className="sizes">
-            {["L", "XL", "M"].map((size, i) => (
-              <button key={i}>{size}</button>
-            ))}
-          </div>
-        </div>
+        {currentImg.listFields.map((listField) => (
+           <div className="section" key={listField.id}>
+           <strong>{listField.label}:</strong>
+           <div className="colors" key={listField.id}>
+             {listField.field.map((type,i) => (
+               <button key={i}>{type}</button>
+             ))}
+           </div>
+         </div>
+        ))}
+        
         <div className="section">
           <strong>Số Lượng:</strong>
           <div className="qty">
-            <button>-</button>
-            <input type="number" defaultValue="1" min="1" style={{ width: "60px", textAlign: "center" }} />
-            <button>+</button>
-            <span>11044 sản phẩm có sẵn</span>
+            <button onClick={() => {
+              let newQty = qty-1;
+              if(newQty>=1){
+                setQty(newQty);
+              }
+            }} >-</button>
+            <input type="number" defaultValue="1" min="1" value={qty} style={{ width: "60px", textAlign: "center" }} />
+            <button onClick={() => {
+              let newQty = qty+1;
+              if(newQty<=qtyLeft){
+                setQty(newQty); 
+              }
+            }}>+</button>
+            <span>{qtyLeft} sản phẩm có sẵn</span>
           </div>
         </div>
         <div className="buy-section">
